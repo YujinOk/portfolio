452f0e7849611027bf5d050b9bd14fc8b63922b2
Yujin Ok <yujinok63@gmail.com>
2026-06-18 08:46:29 +0900

    chore: set site identity and move assets to public

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Yujin Ok';
+export const SITE_DESCRIPTION = 'Developer based in Melbourne. Previously fitness instructor, health scientist, and haematology lab tech.';
